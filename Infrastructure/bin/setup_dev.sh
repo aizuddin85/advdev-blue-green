a68db36fe629af224c823b19596f65a991217a28
aizuddin85 <mzali@redhat.com>
2018-12-15 23:19:10 +0800

    fix typo

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -25,16 +25,16 @@ oc new-app --template=mongodb-persistent --param-file=./Infrastructure/bin/param
 # Create new build and point to Nexus.
 oc new-build --name="mlbparks" --binary=true  jboss-eap70-openshift:1.7 --to=${GUID}-parks-dev/mlbparks:0.0-0 -e MAVEN_MIRROR_URL=http://nexus3-90cd-nexus.apps.na311.openshift.opentlc.com/repository/maven-all-public -n ${GUID}-parks-dev
 # Send binary build to OCP builder.
-oc start-build mlbparks --from-dir=MLBParks -n ${GUID}-parks-dev --follow 
+oc start-build mlbparks --from-dir=MLBParks -n ${GUID}-parks-dev --follow  --wait
 # Always tag the newest version as latest, so there will be easy to manage DC and switching image when necessary.
-oc tag  ${GUID}-parks-dev/mlbparks:0.0-0 ${GUID}-parks-dev/mlbparks:latest n ${GUID}-parks-dev
+oc tag  ${GUID}-parks-dev/mlbparks:0.0-0 ${GUID}-parks-dev/mlbparks:latest -n ${GUID}-parks-dev
 # Application objects definition nicely put in the template.
 oc process -f ./Infrastructure/templates/mlbparks.yaml | oc create -f - -n ${GUID}-parks-dev
 
 # Create new build and point to Nexus.
 oc new-build --name="nationalparks" --binary=true  redhat-openjdk18-openshift:1.2 --to=${GUID}-parks-dev/nationalparks:0.0-0 -e MAVEN_MIRROR_URL=http://nexus3-90cd-nexus.apps.na311.openshift.opentlc.com/repository/maven-all-public -n ${GUID}-parks-dev
 # Send binary build to OCP builder.
-oc start-build nationalparks --from-dir=Nationalparks -n ${GUID}-parks-dev --follow
+oc start-build nationalparks --from-dir=Nationalparks -n ${GUID}-parks-dev --follow --wait
 # Always tag the newest version as latest, so there will be easy to manage DC and switching image when necessary.
 oc tag  ${GUID}-parks-dev/nationalparks:0.0-0 ${GUID}-parks-dev/nationalparks:latest -n ${GUID}-parks-dev
 # Application objects definition nicely put in the template.
@@ -46,7 +46,7 @@ oc policy add-role-to-user view --serviceaccount=default -n  ${GUID}-parks-dev
 # Create new build and point to Nexus.
 oc new-build --binary=true --name="parksmap" redhat-openjdk18-openshift:1.2 --to=${GUID}-parks-dev/parksmap:0.0-0 -e MAVEN_MIRROR_URL=http://nexus3-90cd-nexus.apps.na311.openshift.opentlc.com/repository/maven-all-public -n ${GUID}-parks-dev
 # Send binary build to OCP builder.
-oc start-build parksmap --from-dir=ParksMap -n ${GUID}-parks-dev --follow -n ${GUID}-parks-dev
+oc start-build parksmap --from-dir=ParksMap -n ${GUID}-parks-dev --follow  --wait
 # Always tag the newest version as latest, so there will be easy to manage DC and switching image when necessary
 oc tag ${GUID}-parks-dev/parksmap:0.0-0 ${GUID}-parks-dev/parksmap:latest -n ${GUID}-parks-dev
 # Application objects definition nicely put in the template.
