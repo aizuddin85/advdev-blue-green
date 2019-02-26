73ad74828b367e8a48f1436e4723b105eff60daf
root <root@master01.local.topimerah.net>
2019-02-26 18:38:12 +0800

    comment

--- Infrastructure/bin/setup_projects.sh
+++ Infrastructure/bin/setup_projects.sh
@@ -9,11 +9,11 @@ fi
 GUID=$1
 USER=$2
 echo "Creating all Homework Projects for GUID=${GUID} and USER=${USER}"
-oc new-project ${GUID}-nexus        --display-name="${GUID} AdvDev Homework Nexus"
-oc new-project ${GUID}-sonarqube    --display-name="${GUID} AdvDev Homework Sonarqube"
-oc new-project ${GUID}-jenkins    --display-name="${GUID} AdvDev Homework Jenkins"
-oc new-project ${GUID}-parks-dev  --display-name="${GUID} AdvDev Homework Parks Development"
-oc new-project ${GUID}-parks-prod --display-name="${GUID} AdvDev Homework Parks Production"
+oc new-project ${GUID}-nexus        --display-name="${GUID} AdvAppDev Nexus"
+oc new-project ${GUID}-sonarqube    --display-name="${GUID} AdvAppDev Sonarqube"
+oc new-project ${GUID}-jenkins    --display-name="${GUID} AdvAppDev Jenkins"
+oc new-project ${GUID}-parks-dev  --display-name="${GUID} AdvAppDev Parks Development"
+oc new-project ${GUID}-parks-prod --display-name="${GUID} AdvAppDev Parks Production"
 
 oc policy add-role-to-user admin ${USER} -n ${GUID}-nexus
 oc policy add-role-to-user admin ${USER} -n ${GUID}-sonarqube
